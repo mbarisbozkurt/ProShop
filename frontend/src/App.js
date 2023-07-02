eb4d2deaca802f4fed6db812cb60cf0b094ed7da
Baris Bozkurt <mbaris@sabanciuniv.edu>
2023-07-02 06:15:52 +0300

    DAY 1 - Header and Footer added with React Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './components/Header';
+import {Container} from "react-bootstrap";
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
-    <h1>Welcome to Proshop!</h1>
+    <>
+      <Header />
+        <main className='py-3'> {/*py-3: padding on the y axis*/}
+          <Container>
+            <h1>Welcome To Proshop</h1>
+          </Container>
+        </main>
+      <Footer/>
+    </>
   )
 }
 
