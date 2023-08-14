0747e3a93dcd8b18d7fa643489ef23f41ebdffa4
Baris Bozkurt <mbaris@sabanciuniv.edu>
2023-08-14 15:30:43 +0300

    prepare to production

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({extended: true}));
 //Cookie-parser middleware, allows to req.cookies.jwt (jwt is the name of the cookie) 
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-})
-
 app.use("/api/products", productRoutes); //after /api/products, no matter what is coming after that, go to productRoutes
 app.use("/api/users", userRoutes); 
 app.use("/api/orders", orderRoutes);
@@ -38,6 +34,22 @@ app.get("/api/config/paypal", (req, res) =>
 //to be able to upload image when admin edits the product, not that important
 const __dirname = path.resolve(); //currenct directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+
+//deploy to production
+if(process.env.NODE_ENV === "production"){
+  //If in production mode, serve the frontend's static build files
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => 
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  })
+}
 
 app.use(notFound);
 app.use(errorHandler);
